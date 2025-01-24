2c554584a7474b0a03a0d34e65505576263edf9a
Florian Schrack <32493263+florianschrack@users.noreply.github.com>
2025-01-24 11:25:16 +0100

    Customize main.js

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Florian Schrack",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Build and deployment pipelines", "automation", "DevOps"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Jenkins, Azure DevOps",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Share experiences about different CI/CD solutions"],
   },
 });
