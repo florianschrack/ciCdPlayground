ec78c8bb84ffba2311133bf918d3afe748c95f67
Florian Schrack <32493263+florianschrack@users.noreply.github.com>
2025-08-22 11:10:21 +0200

    Fill out introduction data

--- src/main.js
+++ src/main.js
@@ -4,15 +4,15 @@ new App({
   target: document.body,
   props: {
     // What's your name?
-    name: "James Butler",
+    name: "Florian Schrack",
     // In the following fiels you can either give a single string,
     // or an array of bullet points
 
     // What do you associate with the term 'CI/CD'?
-    associations: ["Ehm, what?"],
+    associations: ["Automation", "Build/deployment pipelines", "Blinking tests"],
     // Which CI/CD tools do you use in your project?
-    tools: "Jenkins, maybe?",
+    tools: "Recently migrated from Jenkins to Azure DevOps",
     // What do you want to learn in this workshop?
-    expectations: ["what you are talking about"],
+    expectations: ["Introduce basic concepts and some CI/CD tools", "Share experience with different CI/CD tools"],
   },
 });
